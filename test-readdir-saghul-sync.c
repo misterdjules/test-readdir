f5caf33af646aaabc8bd42160479129b9cb52cf3
Julien Gilli <julien.gilli@joyent.com>
2014-11-09 22:28:47 -0800

    Fix test-readdir-saghul-sync

--- test-readdir-saghul-sync.c
+++ test-readdir-saghul-sync.c
@@ -13,17 +13,19 @@ int main(int argc, char* argv[]) {
   uv_dir_t    dir;
   uv_dirent_t dirents[NB_ENTRIES];
 
+  int nb_entries_read;
+
   unsigned int dirent_idx;
 
   uv_fs_opendir(uv_default_loop(), &opendir_req, argv[1], NULL);
 
-  while (uv_fs_readdir(uv_default_loop(),
-                       &readdir_req,
-                       opendir_req.dir,
-                       dirents,
-                       NB_ENTRIES,
-                       NULL) != 0) {
-    for (dirent_idx = 0; dirent_idx < NB_ENTRIES; ++dirent_idx) {
+  while ((nb_entries_read = uv_fs_readdir(uv_default_loop(),
+                                          &readdir_req,
+                                          opendir_req.dir,
+                                          dirents,
+                                          NB_ENTRIES,
+                                          NULL)) > 0) {
+    for (dirent_idx = 0; dirent_idx < (unsigned int)nb_entries_read; ++dirent_idx) {
       printf("%s\n", dirents[dirent_idx].name);
     }
   }
